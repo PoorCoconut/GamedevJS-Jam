1006b08c5cef619759c5bb27e8ec6a65905a2cf7
PoorCoconut <cueshealforque@gmail.com>
2026-04-26 18:55:22 +0800

    UI Bar updated

--- systems/HUD/player_hud.gd
+++ systems/HUD/player_hud.gd
@@ -4,24 +4,46 @@ extends CanvasLayer
 @onready var BarChaser = %BarChaser
 var style
 
+var target_fuel: float = 0.0
+var chase_speed: float = 30.0 
+
+var hang_timer: float = 0.0
+var hang_duration: float = 0.4 
+
+var is_initialized: bool = false 
+
 func _ready():
-	style = BarChaser.get_theme_stylebox("fill") as StyleBoxFlat
+	style = WhiteBar.get_theme_stylebox("fill") as StyleBoxFlat
 	Events.player_fuel_updated.connect(_on_player_fuel_updated)
 
 func _on_player_fuel_updated(current_fuel: float, max_fuel: float):
 	WhiteBar.max_value = max_fuel
-	WhiteBar.value = current_fuel
+	BarChaser.max_value = max_fuel
+	target_fuel = current_fuel
 	
-	style.bg_color = Color.RED.lerp(Color.CYAN, current_fuel / max_fuel)
-	var tween = create_tween()
-	tween.tween_property(BarChaser, "value", WhiteBar.value, 0.5).set_trans(Tween.TRANS_SINE)
+	style.bg_color = Color.RED.lerp(Color.CYAN, target_fuel / max_fuel)
+	if not is_initialized:
+		WhiteBar.value = current_fuel
+		BarChaser.value = current_fuel
+		is_initialized = true
+		return
 	
-	if BarChaser.value < 3:
-		BarChaser.value = 0
-	
-	if WhiteBar.value < 25:
-		var tween2 = create_tween()
-		tween2.tween_property($BarContainer, "modulate:a", 1, 0.5)
-	else:
-		var tween3 = create_tween()
-		tween3.tween_property($BarContainer, "modulate:a", 1, 0.5)
+	#Draining
+	if target_fuel < WhiteBar.value:
+		WhiteBar.value = target_fuel
+		hang_timer = hang_duration
+		
+	#Refueling
+	elif target_fuel > BarChaser.value:
+		BarChaser.value = target_fuel
+
+func _process(delta: float):
+	if hang_timer > 0:
+		hang_timer -= delta
+		return
+		
+	if BarChaser.value > target_fuel:
+		BarChaser.value = move_toward(BarChaser.value, target_fuel, chase_speed * delta)
+			
+	elif WhiteBar.value < target_fuel:
+		WhiteBar.value = move_toward(WhiteBar.value, target_fuel, chase_speed * delta)
